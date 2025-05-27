a32687e30f695d494f510cd5804c13d065b21086
choijhn <jhchoi4912@gmail.com>
2025-05-27 19:31:44 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'front',
   plugins: [react()],
   build: {
-    outDir: '../dist',
-    emptyOutDir: true
+    outDir: 'static/dist',
   }
 })
