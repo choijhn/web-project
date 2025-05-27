85e4dbdf651d5348e81c53666c35e7cbaad1c5c6
choijhn <jhchoi4912@gmail.com>
2025-05-27 19:51:44 +0900

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   root: 'front',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  }
-})
+    outDir: '../dist', // 최종 빌드는 프로젝트 루트에 dist 폴더 생성
+  },
+});
